e52b6e6ae87bdfb55ad39f99ebeb9b0ecc6ce982
punyprogrammer <johnmayer190999@gmail.com>
2022-10-20 22:53:36 +0530

    add-login-signup

--- src/components/Navbar/RightContent/RightContent.tsx
+++ src/components/Navbar/RightContent/RightContent.tsx
@@ -1,19 +1,23 @@
-import { Flex } from '@chakra-ui/react';
+import { Button, Flex } from '@chakra-ui/react';
+import { signOut } from 'firebase/auth';
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../../../firebase/clientApp';
 import AuthModal from '../../Modal/Auth/AuthModal';
 import AuthButtons from './AuthButtons';
 
 type RightContentProps = {
-    
+    user:any;
 };
 
-const RightContent:React.FC<RightContentProps> = () => {
+const RightContent:React.FC<RightContentProps> = ({user}) => {
+   
     
     return (
         <>
         <AuthModal/>
         <Flex justify="center" align="center">
-            <AuthButtons/>
+            {user?<Button onClick={()=>signOut(auth)}>Logout</Button>:<AuthButtons/>}
         </Flex>
         </>
     )
