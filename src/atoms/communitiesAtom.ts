2cfb9988529331b64deb693ec295b804c7e25a26
punyprogrammer <johnmayer190999@gmail.com>
2022-10-26 23:44:29 +0530

    add-not-found-community

--- src/atoms/communitiesAtom.ts
+++ src/atoms/communitiesAtom.ts
@@ -1,8 +1,11 @@
+import { Timestamp } from "firebase/firestore";
 import { atom } from "recoil";
 export interface Community{
     id:string,
     creatorId:string,
     numberOfMembers:number;
     privacyType:'public'|'private'|'restricted';
+    createdAt?:Timestamp;
+    imageURL?:string;
     
 }
---
+++ src/components/community/NotFound.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Flex, Button, Text } from "@chakra-ui/react";
+import Link from "next/link";
+
+const CommunityNotFound: React.FC = () => {
+  return (
+    <Flex
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      minHeight="60vh"
+      fontWeight={500}
+    >
+        <Text align="center">
+
+      Sorry, that community does not exist or has been banned
+        </Text>
+      <Link href="/">
+        <Button mt={4}>GO HOME</Button>
+      </Link>
+    </Flex>
+  );
+};
+export default CommunityNotFound;
