1c38eeb2fd209a724fb46f0c97f91b07e5c1a882
punyprogrammer <johnmayer190999@gmail.com>
2022-11-07 01:37:51 +0530

    add-post-item-ui

--- src/atoms/postsAtoms.ts
+++ src/atoms/postsAtoms.ts
@@ -10,7 +10,7 @@ export type Post = {
   body: string
   numberOfComments: number
   voteStatus: number
-  imageUrl?: string
+  imageURL?: string
   communitImageURL?: string
   createdAt: Timestamp
 }
--- src/components/posts/Posts.tsx
+++ src/components/posts/Posts.tsx
@@ -1,9 +1,12 @@
+import { Stack } from '@chakra-ui/react'
 import { collection, getDocs, orderBy, query, where } from 'firebase/firestore'
 import React, { useEffect, useState } from 'react'
+import { useAuthState } from 'react-firebase-hooks/auth'
 import { Community } from '../../atoms/communitiesAtom'
 import { Post } from '../../atoms/postsAtoms'
-import { firestore } from '../../firebase/clientApp'
+import { auth, firestore } from '../../firebase/clientApp'
 import usePosts from '../../hooks/usePosts'
+import PostItem from './PostItem'
 
 type PostsProps = {
   communityData: Community
@@ -11,21 +14,26 @@ type PostsProps = {
 }
 
 const Posts: React.FC<PostsProps> = ({ communityData }) => {
-  const [loading, setLoading] = useState(false);
-  const {postStateValue,setPostStateValue}=usePosts();
+  const [user] = useAuthState(auth)
+  const [loading, setLoading] = useState(false)
+  const {
+    postStateValue,
+    setPostStateValue,
+    onDelete,
+    onSelect,
+    onVote
+  } = usePosts()
   const getPosts = async () => {
     try {
       const postsQuery = query(
-        collection(firestore, "posts"),
-        where("communityId", "==", communityData.id),
-        orderBy("createdAt", "desc")
+        collection(firestore, 'posts'),
+        where('communityId', '==', communityData.id),
+        orderBy('createdAt', 'desc')
       )
       const postDocs = await getDocs(postsQuery)
       // Store in post state
       const posts = postDocs.docs.map(doc => ({ id: doc.id, ...doc.data() }))
-      setPostStateValue((prev)=>(
-        {...prev,posts:posts as Post[]}
-      ))
+      setPostStateValue(prev => ({ ...prev, posts: posts as Post[] }))
     } catch (error:any) {
       console.log('GetPOsts Error', error.message)
     }
@@ -35,9 +43,22 @@ const Posts: React.FC<PostsProps> = ({ communityData }) => {
   }, [])
 
   return (
-    postStateValue.posts.map((post)=>{
-        return <h1>{post.title}</h1>
-    })
+    <Stack>
+      {postStateValue.posts.map(post => {
+        return (
+          <PostItem
+            post={post}
+            userIsCreator={user?.uid===post.creatorId}
+            key={post.id}
+            onVote={onVote}
+            onDeletePost={onDelete}
+            onSelectPost={onSelect}
+            userVoteValue={undefined}
+
+          />
+        )
+      })}
+    </Stack>
   )
 }
 export default Posts
