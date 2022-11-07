0851d8cce45026b99e22b02933a533c4ca72b158
punyprogrammer <johnmayer190999@gmail.com>
2022-11-07 06:00:24 +0530

    add-delete-community-functionality

--- src/hooks/usePosts.tsx
+++ src/hooks/usePosts.tsx
@@ -1,20 +1,47 @@
+import { deleteDoc, doc } from 'firebase/firestore'
+import { deleteObject, ref } from 'firebase/storage'
 import React from 'react'
 import { useRecoilState } from 'recoil'
-import { postState } from '../atoms/postsAtoms'
+import { Post, postState } from '../atoms/postsAtoms'
+import { firestore, storage } from '../firebase/clientApp'
 
 const usePosts = () => {
   const [postStateValue, setPostStateValue] = useRecoilState(postState)
   const onVote = async () => {}
-  const onSelect = () => {}
+  const onSelectPost = () => {}
 
-  const onDelete = async () => {}
+  const onDeletePost = async (post:Post):Promise<boolean> => {
+   try {
+    // check if image is attached with post -->delete from storage
+    if(post.imageURL)
+    {
+      const imageRef=ref(storage,`posts/${post.id}/image`);
+      await deleteObject(imageRef);
+    }
+    // delete post doc from firestore
+    const postDocRef=doc(firestore,'posts',post.id!);
+    await deleteDoc(postDocRef);
+
+
+    // update currentPostStats
+    setPostStateValue(prev=>({...prev,posts:prev.posts.filter(item=>item.id!==post.id)}))
+   }
+    catch (error) {
+    return false;
+   }
+
+
+    return true;
+
+
+  }
 
   return {
     postStateValue,
     setPostStateValue,
     onVote,
-    onSelect,
-    onDelete,
+    onSelectPost,
+    onDeletePost,
   }
 }
 export default usePosts
